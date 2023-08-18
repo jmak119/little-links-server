0e7ad3a0f7dc83242be9e1ed222ada81c53a55c3
Jonathan Mak <j.m4k119@gmail.com>
2023-08-18 16:43:22 -0400

    began add usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,33 @@
-const knex = require('knex') (require('..knexfile'));
+const knex = require('knex')(require('..knexfile'));
 
 const index = (_req, res) => {
     knex('user')
         .then((data) => {
             res.status(200).json(data);
         })
+        .catch((err) =>
+            res.status(400).send(`Error retrieving Users: ${err}`)
+        );
+};
+
+const findOne = (req, res) => {
+    knex("user")
+        .where({ id: req.params.id })
+        .then((usersFound) => {
+
+            if (usersFound.length === 0) {
+                return res
+                    .status(404)
+                    .json({ message: `User with ID: ${req.params.id} not found` });
+            }
+
+            const userData = usersFound[0];
+
+            res.status(200).json(userData);
+        })
+        .catch(() => {
+            res.status(500).json({
+                message: `Unable to retrieve user data for user with ID: ${req.params.id}`,
+            });
+        });
 }
